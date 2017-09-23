cf78eadc5fbf6ab9debb78a4440ae54dc635e0cf
柏正权 <baizhengquan@zhongan.com>
2017-09-23 17:44:39 +0800

    Edit App.js to use css module

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div class={styles.app}>
+        <div class={styles.appHeader}>
+          <img src={logo} class={styles.appLogo} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p class={styles.appIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
