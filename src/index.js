df5c7e6365732973a2c3095bbbff8e1e6c514a30
柏正权 <baizhengquan@zhongan.com>
2017-09-23 16:21:02 +0800

    Fix react-hot-loader usage error

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import './index.css';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
-ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  document.getElementById('app')
-);
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('app')
+  );
+};
+
+render(App);
 
 if (module.hot) {
   module.hot.accept('./App', () => render(App));
